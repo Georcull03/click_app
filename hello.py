8fa3c2830e1e1669fc512ffa1c59f47abb69e357
George Cullender <cullenge@amazon.com>
2023-08-30 12:47:24 +0000

    Added content to hello.py. Also created git ignore file.

---
+++ hello.py
@@ -0,0 +1,9 @@
+import click
+
+@click.command("hello")
+@click.version_option("0.1.0", prog_name="hello")
+def hello():
+    click.echo("Hello, you are using the Python Click module!")
+
+if __name__ == "__main__":
+    hello()

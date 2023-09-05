2866a371d0802e38b9a67e3e3613e2096b0f0da9
George Cullender <cullenge@amazon.com>
2023-09-05 08:36:08 +0000

    Created rectangle_v2.py, finds the size of rectangle taking input from the CLI encapsulating the options within a tuple manually.

--- rectangle_v2.py
+++ rectangle_v2.py
@@ -2,12 +2,12 @@ import click
 
 
 @click.command()
-@click.option("--size", nargs=2, type=click.INT)
+@click.option("--size", type=(click.INT, click.INT))
 def cli(size):
     width, height = size
     click.echo(f"size: {size}")
     click.echo(f"{width} x {height}")
-    click.echo(f"area = {width * height}cm^2")
+    click.echo(f"area: {width * height}cm^2")
 
 
 if __name__ == "__main__":

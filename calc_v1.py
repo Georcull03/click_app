fb9b8d8a778c0035ce41a74951913a8256e4a171
George Cullender <cullenge@amazon.com>
2023-09-19 16:13:17 +0000

    Added other subcommands to the command group giving more functionality to the calculator

--- calc_v1.py
+++ calc_v1.py
@@ -13,5 +13,26 @@ def add(a, b):
     click.echo(f"{a} + {b} = {a + b}")
 
 
+@cli.command()
+@click.argument("a", type=click.FLOAT)
+@click.argument("b", type=click.FLOAT)
+def subtract(a, b):
+    click.echo(f"{a} - {b} = {a - b}")
+
+
+@cli.command()
+@click.argument("a", type=click.FLOAT)
+@click.argument("b", type=click.FLOAT)
+def multiply(a, b):
+    click.echo(f"{a} * {b} = {a * b}")
+
+
+@cli.command()
+@click.argument("a", type=click.FLOAT)
+@click.argument("b", type=click.FLOAT)
+def divide(a, b):
+    click.echo(f"{a} / {b} = {a / b}")
+
+
 if __name__ == "__main__":
     cli()

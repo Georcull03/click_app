e53ab3e59b094cf991b268c1cfdc448dcdd83e6f
George Cullender <cullenge@amazon.com>
2023-09-19 16:01:55 +0000

    Added add function to cli command group.

--- calc_v1.py
+++ calc_v1.py
@@ -4,3 +4,14 @@ import click
 @click.group()
 def cli():
     pass
+
+
+@cli.command()
+@click.argument("a", type=click.FLOAT)
+@click.argument("b", type=click.FLOAT)
+def add(a, b):
+    click.echo(f"{a} + {b} = {a + b}")
+
+
+if __name__ == "__main__":
+    cli()
